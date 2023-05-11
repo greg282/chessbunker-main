142f1602e3a933839581f6a393bde634cee4add0
greg282 <gregor22315@gmail.com>
2023-05-11 19:48:55 +0200

    changed window name

--- client/ptchess/main_frame.py
+++ client/ptchess/main_frame.py
@@ -52,7 +52,7 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("Chessbunker", "Chessbunker"))
 
 
     def make_menubar(self):
